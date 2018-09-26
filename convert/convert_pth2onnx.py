95adf4f15c537814e26a1ecf7f04efd2bc1bc8e9
Manuel Costa <manuel@nativolabs.com>
2018-09-26 10:28:05 +0100

    missing from previous commit

--- convert/convert_pth2onnx.py
+++ convert/convert_pth2onnx.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 import torch
 import torch.onnx
-from torchvision.datasets.folder import default_loader
-from nima.common import Transform
+from torch.autograd import Variable
 
 from nima.inference.inference_model import InferenceModel
 
 
-def convert_pth2onnx(path_to_src_pth, path_to_dest_onnx, verbose = False):
+def convert_pth2onnx(path_to_src_pth, path_to_dest_onnx, vb = False):
     
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
         
     nimaModel = InferenceModel(path_to_model=path_to_src_pth)
 
-    transform = Transform().val_transform
-
-    image = default_loader("Dummy.jpg")
-    image = transform(image)
-    image = image.unsqueeze_(0)
-    image = image.to(device)
-
-    with torch.no_grad():
-        image = torch.autograd.Variable(image)
-
-    torch.onnx.export(nimaModel.model, image, verbose=True)
+    
+    dummy_input = Variable(torch.randn(1, 3, 224, 224))
+    
+    torch.onnx.export(nimaModel.model, dummy_input, path_to_dest_onnx, verbose = vb)
